018e2a4e4ba139b2f88e75171c98d4e8107fba3f
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-06 06:23:34 +0500

    app: wire global smooth scroll and motion system

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { MotionSystem } from './engine/MotionSystem'
+import { SmoothScroll } from './engine/SmoothScroll'
+import 'lenis/dist/lenis.css'
 import './styles.css'
 import './scrolly.css'
+import './premium-scroll.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MotionSystem>
+      <SmoothScroll>
+        <App />
+      </SmoothScroll>
+    </MotionSystem>
   </StrictMode>,
 )
