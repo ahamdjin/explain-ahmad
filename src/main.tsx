6d8c28b1f6e92b6d3d06bc4c8c3fdd672d0d6679
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-06 06:32:50 +0500

    architecture: remove global motion and smooth-scroll wrappers

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { MotionSystem } from './engine/MotionSystem'
-import { SmoothScroll } from './engine/SmoothScroll'
-import 'lenis/dist/lenis.css'
 import './styles.css'
 import './scrolly.css'
 import './premium-scroll.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MotionSystem>
-      <SmoothScroll>
-        <App />
-      </SmoothScroll>
-    </MotionSystem>
+    <App />
   </StrictMode>,
 )
