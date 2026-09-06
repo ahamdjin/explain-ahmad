fe66fc503766f7788fbd5eaf5cf96e1853654b9c
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-06 07:55:54 +0500

    app: enable browser routing and design system

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import App from './App'
 import './styles.css'
 import './scrolly.css'
 import './premium-scroll.css'
 import './premium-patterns.css'
+import './design-system.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
